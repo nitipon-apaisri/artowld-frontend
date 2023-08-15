d91617324aaf1e26f227a87ba0d7385ecfbb8d2e
nitipon <nitipon.apaisri@gmail.com>
2023-08-15 16:17:44 +0200

    Updated the footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import logo from "../assets/images/logo.svg";
+const Footer = () => {
+    return (
+        <footer className="mt-40">
+            <div className="footer p-10 bg-[#FAFAFA]">
+                <div className="flex flex-row justify-between items-center ">
+                    <div className="logo flex flex-row items-center space-x-4">
+                        <img src={logo} alt="App-logo" />
+                        <h1 className="font-bold tracking-wider">ARTOWLD</h1>
+                    </div>
+                    <h5>Developed by Unagi Studio</h5>
+                    <div></div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { layoutProps } from "../types/types";
 import Header from "../components/Header";
-import Footer from "./Footer";
+import Footer from "../components/Footer";
 
 const AppLayout: React.FC<layoutProps> = ({ children }) => {
     return (
