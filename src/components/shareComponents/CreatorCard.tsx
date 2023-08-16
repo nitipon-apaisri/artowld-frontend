7d1168b3ca8984150db15f53d103729eced2f380
nitipon <nitipon.apaisri@gmail.com>
2023-08-16 10:44:58 +0200

    Added reponsive design to 'CreatorCard' component

--- src/components/shareComponents/CreatorCard.tsx
+++ src/components/shareComponents/CreatorCard.tsx
@@ -3,12 +3,12 @@ import { creatorCardProps } from "../../types/types";
 
 const CreatorCard: React.FC<creatorCardProps> = ({ username }) => {
     return (
-        <div className="w-100 aspect-4/3 rounded-2xl border border-slate-300 relative hover:shadow-custom2 duration-500">
-            <div className="w-full h-[60%] bg-slate-500 rounded-t-2xl"></div>
-            <div className="absolute -mr-[50%] left-2/4 -translate-x-1/2 top-[45%]">
-                <div className="profile_image w-[128px] aspect-square bg-slate-500 outline outline-8 outline-white"></div>
-                <div className="text-center mt-6">
-                    <h5 className="font-bold tracking-wider">{username}</h5>
+        <div className="w-100 aspect-square xl:aspect-4/3 rounded-2xl border border-slate-300 relative hover:shadow-custom2 duration-500">
+            <div className="w-full h-[50%]  bg-slate-500 rounded-t-2xl"></div>
+            <div className="absolute -mr-[50%] left-2/4 -translate-x-1/2 -translate-y-1/2 top-[calc(50%+24px)] xl:top-[calc(50%+32px)]">
+                <div className="profile_image 2xl:w-[128px] xl:w-24 w-20 mx-auto aspect-square bg-slate-500 outline outline-8 outline-white"></div>
+                <div className="text-center mt-6 xl:mt-8">
+                    <h5 className="font-bold tracking-wider text-lg xl:text-xl 2xl:text-2xl">{username}</h5>
                 </div>
             </div>
         </div>
