4b0030dc91619ba89f164a7da058ef6e1a6cd0e3
nitipon <nitipon.apaisri@gmail.com>
2023-08-16 08:10:51 +0200

    Added reponsive design to 'CardOfTheDay' component

--- src/components/CardOfTheDay.tsx
+++ src/components/CardOfTheDay.tsx
@@ -1,14 +1,14 @@
 // import img from "../assets/images/test.jpg";
 const CardOfDay = () => {
     return (
-        <div className="w-[480px] h-[480px] rounded-2xl shadow-custom">
+        <div className="2xl:w-[480px] 2xl:h-[480px] w-[400px] h-[400px] rounded-2xl shadow-custom">
             <div className="w-full aspect-square bg-slate-500 rounded-2xl">
-                <div className="w-11/12   bg-white rounded-2xl absolute left-2/4 -mr-[50%] -translate-x-1/2 bottom-6">
-                    <div className="flex flex-row items-center justify-between p-6">
+                <div className="w-11/12 bg-white rounded-2xl absolute left-2/4 -mr-[50%] -translate-x-1/2 2xl:bottom-6 bottom-4">
+                    <div className="flex flex-row items-center justify-between 2xl:p-6 p-4">
                         <div className="flex flex-row items-center">
                             <div className="profile_image w-12 h-12  bg-slate-500"></div>
                             <div className="ml-4">
-                                <h5 className="font-bold tracking-wide antialiased">Lorem</h5>
+                                <h5 className="font-bold tracking-wide antialiased ">Lorem</h5>
                                 <p className="text-xs text-primary font-medium tracking-wider">Username</p>
                             </div>
                         </div>
