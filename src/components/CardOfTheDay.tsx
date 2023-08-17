f1fd8ed9655bfbd8727fdcd6b478165b978360f5
nitipon <nitipon.apaisri@gmail.com>
2023-08-17 22:27:08 +0200

    Updated responsive design

--- src/components/CardOfTheDay.tsx
+++ src/components/CardOfTheDay.tsx
@@ -1,7 +1,7 @@
 // import img from "../assets/images/test.jpg";
 const CardOfDay = () => {
     return (
-        <div className="w-80 max-[959px]:w-[400px] aspect-square  lg:w-[400px]  2xl:w-[480px]  rounded-2xl shadow-custom">
+        <div className="max-[480px]:w-80 w-[400px] aspect-square  lg:w-[400px]  2xl:w-[480px]  rounded-2xl shadow-custom">
             <div className="w-full aspect-square bg-slate-500 rounded-2xl">
                 <div className="w-11/12 bg-white rounded-2xl absolute left-2/4 -mr-[50%] -translate-x-1/2 2xl:bottom-6 bottom-4">
                     <div className="flex flex-row items-center justify-between 2xl:p-6 p-4">
--- src/components/shareComponents/CreatorCard.tsx
+++ src/components/shareComponents/CreatorCard.tsx
@@ -3,10 +3,10 @@ import { creatorCardProps } from "../../types/types";
 
 const CreatorCard: React.FC<creatorCardProps> = ({ username }) => {
     return (
-        <div className="w-[400px] max-sm:w-[400px] min-[959px]:w-full aspect-square xl:aspect-4/3 rounded-2xl border border-slate-300 relative hover:shadow-custom2 duration-500">
+        <div className="w-80 sm:w-[400px] min-[959px]:w-full aspect-square xl:aspect-4/3 rounded-2xl border border-slate-300 relative hover:shadow-custom2 duration-500">
             <div className="w-full h-[50%]  bg-slate-500 rounded-t-2xl"></div>
             <div className="absolute -mr-[50%] left-2/4 -translate-x-1/2 -translate-y-1/2 top-[calc(50%+24px)] xl:top-[calc(50%+32px)]">
-                <div className="profile_image max-sm:w-32 w-2/4 sm:w-4/5 lg:w-24 2xl:w-32 mx-auto aspect-square bg-slate-500 outline outline-8 outline-white"></div>
+                <div className="profile_image w-2/4  max-sm:w-24  sm:w-11/12 lg:w-24 2xl:w-32 mx-auto aspect-square bg-slate-500 outline outline-8 outline-white"></div>
                 <div className="text-center mt-6 xl:mt-8">
                     <h5 className="font-bold tracking-wider text-lg lg:text-lg xl:text-xl 2xl:text-2xl">{username}</h5>
                 </div>
