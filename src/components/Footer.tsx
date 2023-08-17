f138103e39c08278d185284a17f328f90878a783
nitipon <nitipon.apaisri@gmail.com>
2023-08-17 16:08:26 +0200

    Updated responsive design to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ const Footer = () => {
     return (
         <footer className="mt-40">
             <div className="footer p-10 bg-[#FAFAFA]">
-                <div className="flex flex-row justify-between items-center ">
+                <div className="flex flex-col md:flex-row justify-between items-center ">
                     <div className="logo flex flex-row items-center space-x-4">
                         <img src={logo} alt="App-logo" />
                         <h1 className="font-bold tracking-wider">ARTOWLD</h1>
                     </div>
-                    <h5>Developed by Unagi Studio</h5>
+                    <h5 className="mt-4 md:mt-0">Developed by Unagi Studio</h5>
                     <div></div>
                 </div>
             </div>
