a98051f46108755651f56ebcceb1006d75c5b166
nitipon <nitipon.apaisri@gmail.com>
2023-08-25 22:42:00 +0200

    Update style for 'Header'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,8 @@ const Header = () => {
     const [showLanguages, setShowLanguages] = useState<boolean>(false);
     const [languageDropdown, setLanguageDropdown] = useState<boolean>(false);
     const toggleUserDropdown = () => {
-        setUserDropdown(!toggleUserDropdown);
+        setUserDropdown(!userDropdown);
+        setLanguageDropdown(false);
     };
     const toggleSidebar = () => {
         setIsSidebarToggle(!isSidebarToggle);
@@ -55,40 +56,35 @@ const Header = () => {
                     <h1 className="font-bold tracking-wider">ARTOWLD</h1>
                 </div>
                 <div className="menu_and_user flex flex-row items-center ">
-                    {token !== null ? (
-                        <div className="relative">
-                            <div className="user md:block hidden bg-slate-500 rounded-full w-10 h-10 ml-20 cursor-pointer" onClick={toggleUserDropdown}></div>
-                            {userDropdown && <UserDropDown />}
-                        </div>
-                    ) : (
+                    {innerWidth >= breakpoint && (
                         <>
-                            {innerWidth >= breakpoint && (
-                                <>
-                                    <nav className={`md:mr-4`}>
-                                        <ul className="flex flex-row space-x-6 font-medium tracking-wider">
-                                            <li>
-                                                <a href="/">{t("Home")}</a>
-                                            </li>
-                                            <li>
-                                                <a href="/">{t("Explore")}</a>
-                                            </li>
-                                            <li>
-                                                <a href="/">{t("Stats")}</a>
-                                            </li>
-                                            <li>
-                                                <a href="/">{t("Resources")}</a>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                    <div className="flex flex-row items-center space-x-4">
-                                        <div className="relative">
-                                            <FontAwesomeIcon icon={faLanguage} size="xl" className="cursor-pointer" onClick={toggleLanguageDropdown} />
-                                            {languageDropdown && <LanguageDropdown />}
-                                        </div>
-                                        <SignUpButton />
-                                        <SignInButton />
-                                    </div>
-                                </>
+                            <nav className={`md:mr-4`}>
+                                <ul className="flex flex-row space-x-6 font-medium tracking-wider">
+                                    <li>
+                                        <a href="/">{t("Home")}</a>
+                                    </li>
+                                    <li>
+                                        <a href="/">{t("Explore")}</a>
+                                    </li>
+                                    <li>
+                                        <a href="/">{t("Stats")}</a>
+                                    </li>
+                                    <li>
+                                        <a href="/">{t("Resources")}</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                            <div className="flex flex-row items-center space-x-4">
+                                <div className="relative">
+                                    <FontAwesomeIcon icon={faLanguage} size="xl" className="cursor-pointer" onClick={toggleLanguageDropdown} />
+                                    {languageDropdown && <LanguageDropdown />}
+                                </div>
+                            </div>
+                            {token !== null && (
+                                <div className="relative">
+                                    <div className="user md:block hidden bg-slate-500 rounded-full w-10 h-10 ml-10 cursor-pointer" onClick={toggleUserDropdown}></div>
+                                    {userDropdown && <UserDropDown />}
+                                </div>
                             )}
                         </>
                     )}
@@ -125,7 +121,7 @@ const Header = () => {
                             </div>
                         </>
                     )}
-                    <hr className="mt-4" />
+                    <hr />
                     <div>
                         <p className="mt-4" onClick={() => setShowLanguages(!showLanguages)}>
                             Switch language : <span className="font-extrabold text-lg">{lang === "en" ? "English" : "ไทย"}</span>
