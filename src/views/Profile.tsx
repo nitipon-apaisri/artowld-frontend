8ff098eff9c6d930602ec7691c9cb9d74b9b9c3a
nitipon <nitipon.apaisri@gmail.com>
2023-08-28 08:58:24 +0200

    Updatet responsive on 'Profile' view

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -9,6 +9,27 @@ const Profile = () => {
         setPreviusTab(activeTab);
         setActiveTab(tab);
     };
+    const ProfileBio = () => {
+        return (
+            <div className="profile_bio_content mt-10 max-h-20 overflow-auto no-scrollbar">
+                <p className="text-slate-400 text-sm font-medium antialiased tracking-wide">
+                    ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque dignissim enim sit amet
+                    venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat
+                </p>
+            </div>
+        );
+    };
+    const ExternalLinks = () => {
+        return (
+            <div className="flex justify-end">
+                <div className="external_links ">
+                    <div className="w-8 aspect-square bg-slate-400 rounded-full"></div>
+                    <div className="w-8 aspect-square bg-slate-400 rounded-full"></div>
+                    <div className="w-8 aspect-square bg-slate-400 rounded-full"></div>
+                </div>
+            </div>
+        );
+    };
     useEffect(() => {
         const tab = document.querySelector(`.tab${activeTab}`) as HTMLElement;
         tab.classList.add("activeTab");
@@ -24,25 +45,13 @@ const Profile = () => {
                 <div className="wrapper">
                     <section>
                         <div className="profile_info">
-                            <div className="profile_media">
-                                <div className="profile_image w-60 rounded-full  mx-auto aspect-square bg-slate-500 outline outline-[10px] outline-white"></div>
+                            <div className="profile_media max-[1366px]:mt-10    ">
+                                <div className="profile_image w-60 max-[1366px]:w-40 rounded-full  mx-auto aspect-square bg-slate-500 outline outline-[10px] outline-white"></div>
                                 <h4 className="text-center text-xl mt-8 font-bold">John Doe</h4>
                             </div>
                             <div className="profile_bio">
-                                <div className="flex justify-end">
-                                    <div className="external_links ">
-                                        <div className="w-8 aspect-square bg-slate-400 rounded-full"></div>
-                                        <div className="w-8 aspect-square bg-slate-400 rounded-full"></div>
-                                        <div className="w-8 aspect-square bg-slate-400 rounded-full"></div>
-                                    </div>
-                                </div>
-                                <div className="profile_bio_content mt-10 max-h-20 overflow-auto no-scrollbar">
-                                    <p className="text-slate-400 text-sm font-medium antialiased tracking-wide">
-                                        ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque
-                                        dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in
-                                        pellentesque massa placerat
-                                    </p>
-                                </div>
+                                <ExternalLinks />
+                                <ProfileBio />
                             </div>
                         </div>
                     </section>
