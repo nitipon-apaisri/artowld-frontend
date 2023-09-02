61842c7d7f51e41b5e568ac88be0ffbb5e37da10
nitipon <nitipon.apaisri@gmail.com>
2023-09-02 22:04:02 +0200

    added loader to Marketplace view

--- src/views/Marketplace.tsx
+++ src/views/Marketplace.tsx
@@ -2,9 +2,15 @@ import { useTranslation } from "react-i18next";
 import { sampleCollectedProducts } from "../data/sample";
 import { productCardProps } from "../types/types";
 import ProductCard from "../components/card/ProductCard";
+import { useState } from "react";
+import Loader from "../components/shareComponents/Loader";
 
 const Marketplace = () => {
     const { t } = useTranslation();
+    const [loading, setLoading] = useState<boolean>(true);
+    setTimeout(() => {
+        setLoading(false);
+    }, 300);
     return (
         <main>
             <div className="wrapper">
@@ -12,11 +18,15 @@ const Marketplace = () => {
                     <div className="marketplace_header">
                         <h1>{t("marketplace")}</h1>
                     </div>
-                    <div className="product_grid">
-                        {sampleCollectedProducts.map((product: productCardProps, index: number) => (
-                            <ProductCard product={product.product} key={index} />
-                        ))}
-                    </div>
+                    {loading ? (
+                        <Loader />
+                    ) : (
+                        <div className="product_grid">
+                            {sampleCollectedProducts.map((product: productCardProps, index: number) => (
+                                <ProductCard product={product.product} key={index} />
+                            ))}
+                        </div>
+                    )}
                 </section>
             </div>
         </main>
