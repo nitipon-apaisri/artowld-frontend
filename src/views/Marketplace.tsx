04e291a3f5a8b00a3bf501a1dd2a04601e569401
nitipon <nitipon.apaisri@gmail.com>
2023-09-02 21:43:19 +0200

    Added grid to 'Marketplace' view

--- src/views/Marketplace.tsx
+++ src/views/Marketplace.tsx
@@ -1,4 +1,7 @@
 import { useTranslation } from "react-i18next";
+import { sampleCollectedProducts } from "../data/sample";
+import { productCardProps } from "../types/types";
+import ProductCard from "../components/card/ProductCard";
 
 const Marketplace = () => {
     const { t } = useTranslation();
@@ -8,7 +11,11 @@ const Marketplace = () => {
                 <section>
                     <div className="marketplace_header">
                         <h1>{t("marketplace")}</h1>
-                        <div></div>
+                    </div>
+                    <div className="product_grid">
+                        {sampleCollectedProducts.map((product: productCardProps, index: number) => (
+                            <ProductCard product={product.product} key={index} />
+                        ))}
                     </div>
                 </section>
             </div>
