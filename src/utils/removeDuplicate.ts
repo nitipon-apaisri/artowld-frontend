8429069248da0b967477a2223c2517b691b54a16
nitipon <nitipon.apaisri@gmail.com>
2023-09-08 22:21:53 +0200

    Performed and update styling

--- src/utils/removeDuplicate.ts
+++ src/utils/removeDuplicate.ts
@@ -1,4 +1,4 @@
-const removeDuplicate = (arr: [], key: string) => {
+const removeDuplicate = (arr: never[], key: string) => {
     const removeDuplicate = [...new Map(arr.map((item) => [item[key], item])).values()];
     return removeDuplicate;
 };
